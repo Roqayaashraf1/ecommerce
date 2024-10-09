0d3209a01966035a181f2383f38f8191257778b6
roqaya ashraf <your_email@example.com>
2024-10-09 23:04:52 +0300

    mongoatlas

--- dataBase/dbConnections.js
+++ dataBase/dbConnections.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 export const dbConnection = ()=>{
     mongoose.set('strictQuery', false)
-mongoose.connect('mongodb://127.0.0.1:27017/Ecommerce').then(()=>{
+mongoose.connect('mongodb+srv://roqayaashraf25:UZhrp4LLF2wVbnAm@cluster0.8y9xd.mongodb.net/').then(()=>{
     console.log('database connceted')
 }).catch((err)=>{
     console.log('not connected')
